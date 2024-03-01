e8ec31301621dead5174f37a9f8580f62353faf6
Yaroslav Dernov <yaroslav.dernov@russianpost.ru>
2024-03-01 20:56:20 +0400

    Changed package.json's "type" option to "module", removed "module" option. Updated tsup config - removed cjs from build formats, added minify

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/TypingEffect/index.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
+  format: ["esm"],
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
   sourcemap: true,
   clean: true,
   platform: "browser",
+  minify: true,
 });
