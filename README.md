412579163ac4ccd4471a67bd898a98299f5179e0
Yaroslav Dernov <yaroslav.dernov@russianpost.ru>
2024-03-04 17:05:49 +0400

    Updated main.ts with examples.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-## Getting Started
+# Typing Effect
+
+## Description
+
+A brief description of your project.
+
+## Installation
+
+Instructions on how to install your project.
+
+## Usage
+
+Instructions on how to use your project.
+
+## Contributing
+
+Guidelines for contributing to your project.
+
+## License
+
+Information about the license.
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,6 @@
 import "./style.css";
 import { TypingEffect } from "./TypingEffect";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <div id="catsDiv">Some facts about cats: <span></span><span class='cursor idle'></span></div>
-  </div>
-`;
-
-const [catsSpan, cursor] = document.querySelectorAll<HTMLSpanElement>(
-  "#catsDiv span"
-)! as unknown as [HTMLSpanElement, HTMLSpanElement];
-
 const catFunFacts = [
   "Cats have five toes on their front paws but only four on their back ones.",
   "A group of cats is called a clowder.",
@@ -21,94 +11,419 @@ const catFunFacts = [
   "Cats have a special reflective layer behind their retinas called the tapetum lucidum, which enhances their night vision.",
 ];
 
-const te = new TypingEffect(
-  // catFunFacts,
-  undefined,
-  // (string) => {
-  //   catsSpan.innerText = string;
-  // },
-  undefined,
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div id="typing-examples">
+    <div id="default" class="example-block">
+      <h2>Default example:</h2>
+      <div class="typing-container">Some facts about cats: <span></span></div>
+    </div>
+
+    <div id="typing-delay" class="example-block">
+      <h2>Typing delay examples:</h2>
+      <div>
+        <h3>Default (100ms)</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+      <div>
+        <h3>500ms</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+      <div>
+        <h3>0ms</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+    </div>
+
+    <div id="typing-variation" class="example-block">
+      <h2>Typing variation examples:</h2>
+      <div>
+        <h3>Default (100ms)</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+      <div>
+        <h3>500ms</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+      <div>
+        <h3>0ms</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+      <div>
+        <h3>0ms with typingDelay = 0ms</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+    </div>
+
+    <div id="untyping-delay" class="example-block">
+      <h2>Untyping delay examples:</h2>
+      <div>
+        <h3>Default (100ms)</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+      <div>
+        <h3>500ms</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+      <div>
+        <h3>0ms</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+    </div>
+
+    <div id="delay-before-typing" class="example-block">
+      <h2>Delay before typing examples:</h2>
+      <div>
+        <h3>Default (1600ms)</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+      <div>
+        <h3>10000ms</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+      <div>
+        <h3>0ms</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+    </div>
+
+    <div id="delay-after-typing" class="example-block">
+      <h2>Delay after typing examples:</h2>
+      <div>
+        <h3>Default (3000ms)</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+      <div>
+        <h3>10000ms</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+      <div>
+        <h3>0ms</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+    </div>
+
+    <div id="without-untyping" class="example-block">
+      <h2>No untyping example:</h2>
+      <div class="typing-container">Some facts about cats: <span></span></div>
+    </div>
+
+    <div id="without-cursor" class="example-block">
+      <h2>No cursor example:</h2>
+      <div class="typing-container">Some facts about cats: <span></span></div>
+    </div>
+
+    <div id="cursor-blink-rate" class="example-block">
+      <h2>Cursor blink rate examples:</h2>
+      <div>
+        <h3>Default (500ms)</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+      <div>
+        <h3>1000ms</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+      <div>
+        <h3>100ms</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+    </div>
+
+    <div id="cursor-symbol" class="example-block">
+      <h2>Cursor symbol examples with UTF-8 emoji:</h2>
+      <div>
+        <h3>Typing - 👉; untyping - 👈; blinking - ✋</h3>
+        <div class="typing-container">Some facts about cats: <span></span></div>
+      </div>
+    </div>
+
+    <div id="custom-cursor" class="example-block">
+      <h2>Custom cursor example:</h2>
+
+      <div class="typing-container">Some facts about cats: <span></span><span class='cursor idle'></span></div>
+    </div>
+    
+  </div>
+`;
+
+// default
+const defaultExample = document.querySelectorAll<HTMLSpanElement>(
+  "#default .typing-container span"
+)! as unknown as [HTMLSpanElement];
+
+const defaultTE = new TypingEffect(catFunFacts, (string) => {
+  defaultExample[0].innerText = string;
+}).start();
+
+// typingDelay
+const typingDelayExamples = document.querySelectorAll<HTMLSpanElement>(
+  "#typing-delay .typing-container span"
+)! as unknown as [HTMLSpanElement, HTMLSpanElement, HTMLSpanElement];
+
+const typingDelayTE_default = new TypingEffect(catFunFacts, (string) => {
+  typingDelayExamples[0].innerText = string;
+}).start();
+
+const typingDelayTE_500ms = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    typingDelayExamples[1].innerText = string;
+  },
   {
-    // typingDelay: 0,
-    // typingVariation: 200,
-    // delayBeforeTyping: 0,
-    // delayBeforeUntyping: 0,
-    // untypingDelay: 0,
-    // loop: false,
-    // untypeString: false,
+    typingDelay: 500,
+  }
+).start();
+
+const typingDelayTE_0ms = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    typingDelayExamples[2].innerText = string;
+  },
+  {
+    typingDelay: 0,
+  }
+).start();
+
+// typingVariation
+const typingVariationExamples = document.querySelectorAll<HTMLSpanElement>(
+  "#typing-variation .typing-container span"
+)! as unknown as [
+  HTMLSpanElement,
+  HTMLSpanElement,
+  HTMLSpanElement,
+  HTMLSpanElement
+];
+
+const typingVariationTE_default = new TypingEffect(catFunFacts, (string) => {
+  typingVariationExamples[0].innerText = string;
+}).start();
+
+const typingVariationTE_500ms = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    typingVariationExamples[1].innerText = string;
+  },
+  {
+    typingVariation: 500,
+  }
+).start();
+
+const typingVariationTE_0ms = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    typingVariationExamples[2].innerText = string;
+  },
+  {
+    typingVariation: 0,
+  }
+).start();
+
+const typingVariationTE_0_0ms = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    typingVariationExamples[3].innerText = string;
+  },
+  {
+    typingVariation: 0,
+    typingDelay: 0,
+  }
+).start();
+
+// untypingDelay
+const untypingDelayExamples = document.querySelectorAll<HTMLSpanElement>(
+  "#untyping-delay .typing-container span"
+)! as unknown as [HTMLSpanElement, HTMLSpanElement, HTMLSpanElement];
+
+const untypingDelayTE_default = new TypingEffect(catFunFacts, (string) => {
+  untypingDelayExamples[0].innerText = string;
+}).start();
+
+const untypingDelayTE_500ms = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    untypingDelayExamples[1].innerText = string;
+  },
+  {
+    untypingDelay: 500,
+  }
+).start();
+
+const untypingDelayTE_0ms = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    untypingDelayExamples[2].innerText = string;
+  },
+  {
+    untypingDelay: 0,
+  }
+).start();
+
+// delayBeforeTyping
+const delayBeforeTypingExamples = document.querySelectorAll<HTMLSpanElement>(
+  "#delay-before-typing .typing-container span"
+)! as unknown as [HTMLSpanElement, HTMLSpanElement, HTMLSpanElement];
+
+const delayBeforeTypingTE_default = new TypingEffect(catFunFacts, (string) => {
+  delayBeforeTypingExamples[0].innerText = string;
+}).start();
+
+const delayBeforeTypingTE_10000ms = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    delayBeforeTypingExamples[1].innerText = string;
+  },
+  {
+    delayBeforeTyping: 10000,
+  }
+).start();
+
+const delayBeforeTypingTE_0ms = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    delayBeforeTypingExamples[2].innerText = string;
+  },
+  {
+    delayBeforeTyping: 0,
+  }
+).start();
+
+// delayAfterTyping
+const delayAfterTypingExamples = document.querySelectorAll<HTMLSpanElement>(
+  "#delay-after-typing .typing-container span"
+)! as unknown as [HTMLSpanElement, HTMLSpanElement, HTMLSpanElement];
+
+const delayAfterTypingTE_default = new TypingEffect(catFunFacts, (string) => {
+  delayAfterTypingExamples[0].innerText = string;
+}).start();
+
+const delayAfterTypingTE_10000ms = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    delayAfterTypingExamples[1].innerText = string;
+  },
+  {
+    delayAfterTyping: 10000,
+  }
+).start();
+
+const delayAfterTypingTE_0ms = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    delayAfterTypingExamples[2].innerText = string;
+  },
+  {
+    delayAfterTyping: 0,
+  }
+).start();
+
+// no untyping
+const noUntypingExample = document.querySelectorAll<HTMLSpanElement>(
+  "#without-untyping .typing-container span"
+)! as unknown as [HTMLSpanElement];
+
+const noUntypingTE = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    noUntypingExample[0].innerText = string;
+  },
+  {
+    untypeString: false,
+  }
+).start();
+
+// no cursor
+const noCursorExample = document.querySelectorAll<HTMLSpanElement>(
+  "#without-cursor .typing-container span"
+)! as unknown as [HTMLSpanElement];
+
+const noCursorTE = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    noCursorExample[0].innerText = string;
+  },
+  {
     showCursor: false,
   }
-  // {
-  //   // typingDelay: 0,
-  //   // typingVariation: 0,
-  //   // untypingDelay: 0,
-  //   untypeString: true,
-  // }
-)
-  .setCallback((string) => {
-    catsSpan.innerText = string;
-  })
-  .setStrings(catFunFacts)
+).start();
 
-  .start();
+// cursorBlinkRate
+const cursorBlinkRateExamples = document.querySelectorAll<HTMLSpanElement>(
+  "#cursor-blink-rate .typing-container span"
+)! as unknown as [HTMLSpanElement, HTMLSpanElement, HTMLSpanElement];
 
-te.#runningState = "afterTyping";
+const cursorBlinkRateTE_default = new TypingEffect(catFunFacts, (string) => {
+  cursorBlinkRateExamples[0].innerText = string;
+}).start();
 
-const g = te.onBeforeTyping((i) => {
-  console.log("onBeforeTyping", i);
-  // cursor.classList.remove("idle");
+const cursorBlinkRateTE_10000ms = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    cursorBlinkRateExamples[1].innerText = string;
+  },
+  {
+    cursorBlinkRate: 1000,
+  }
+).start();
 
-  cursor.classList.add("green");
+const cursorBlinkRateTE_100ms = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    cursorBlinkRateExamples[2].innerText = string;
+  },
+  {
+    cursorBlinkRate: 100,
+  }
+).start();
+
+// cursorSymbol
+const cursorSymbolExample = document.querySelectorAll<HTMLSpanElement>(
+  "#cursor-symbol .typing-container span"
+)! as unknown as [HTMLSpanElement];
+
+const cursorSymbolTE = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    cursorSymbolExample[0].innerText = string;
+  },
+  {
+    cursorSymbol: {
+      typing: "👉",
+      untyping: "👈",
+      blinking: "✋",
+    },
+  }
+).start();
+
+// custom cursor
+const customCursorExample = document.querySelectorAll<HTMLSpanElement>(
+  "#custom-cursor .typing-container span"
+)! as unknown as [HTMLSpanElement, HTMLSpanElement];
+
+const customCursorTE = new TypingEffect(
+  catFunFacts,
+  (string) => {
+    customCursorExample[0].innerText = string;
+  },
+  {
+    showCursor: false,
+  }
+).start();
+
+customCursorTE.onBeforeTyping(() => {
+  customCursorExample[1].classList.remove("idle");
 });
 
-const gg = te.onAfterTyping((i) => {
-  console.log("onAfterTyping", i);
-  cursor.classList.add("idle");
-  cursor.classList.remove("green");
+customCursorTE.onAfterTyping(() => {
+  customCursorExample[1].classList.add("idle");
 });
 
-const ggg = te.onBeforeUntyping((i) => {
-  console.log("onBeforeUntyping", i);
-  // cursor.classList.remove("idle");
-  cursor.classList.add("red");
+customCursorTE.onBeforeUntyping(() => {
+  customCursorExample[1].classList.remove("idle");
 });
 
-const gggg = te.onAfterUntyping((i) => {
-  console.log("onAfterUntyping", i);
-  cursor.classList.add("idle");
-  cursor.classList.remove("red");
+customCursorTE.onAfterUntyping(() => {
+  customCursorExample[1].classList.add("idle");
 });
 
-const ggggg = te.onArrayFinished(() => {
-  console.log("onArrayFinished");
-});
-
-// te.onBeforeUntyping((i) => {
-//   console.log("onBeforeUntyping", i);
+// const ggggg = te.onArrayFinished(() => {
+//   console.log("onArrayFinished");
 // });
-
-// te.onArrayFinished((i) => {
-//   console.log("onArrayFinished", i);
-//   // te.start();
-//   setTimeout(() => {
-//     te.start();
-//   }, 9000);
-// }, true);
-
-// const cl = te.onStringStart((rf) => {
-//   console.log("this is before the string typing", rf);
-// });
-
-setTimeout(() => {
-  // g();
-  // te.pause();
-  // console.log("before the string typing cleanup after 20sec");
-  // cl();
-  // te.setStrings(["A new set of strings", "Another one"], true).setOptions({
-  //   typingDelay: 0,
-  //   typingVariation: 0,
-  //   loop: true,
-  // });
-  // te.setStrings([]);
-  // te.setOptions({ untypeString: false }, true);
-}, 4000);
