742b169e019e3dfcd45f54c309bfe730140f377b
Yaroslav <43650170+ydernov@users.noreply.github.com>
2024-03-29 18:46:31 +0400

    Update vite.config.ts
    Changed json to json-summary as reporter

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     coverage: {
-      reporter: ["text", "json", "html"],
+      reporter: ["text", "json-summary", "html"],
       exclude: ["src/main.ts", "**/types.ts", "**/*.d.ts"],
     },
   },
